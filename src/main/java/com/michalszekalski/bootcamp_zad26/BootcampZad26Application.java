9269de9c1bcdcb844b6844a47579ea9dc2cfe482
Michal Szekalski <szekalski.michal@gmail.com>
2023-01-11 22:15:20 +0100

    new project

---
+++ src/main/java/com/michalszekalski/bootcamp_zad26/BootcampZad26Application.java
@@ -0,0 +1,13 @@
+package com.michalszekalski.bootcamp_zad26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampZad26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampZad26Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/michalszekalski/bootcamp_zad26/BootcampZad26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalszekalski.bootcamp_zad26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampZad26ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
